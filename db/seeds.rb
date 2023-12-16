c4d76fc1f348f09c86b1872576e5b54ceb6f5b15
0902-max <moche.0830@icloud.com>
2023-12-16 22:22:50 +0900

    seedで初期データ入力

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+questions_data = [
+  { body: "次の2択から自分にあった方を選んでください" },
+  { body: "自分に当てはまる方を選んでください" },
+  { body: "自分に当てはまる方を選んでください" },
+  # Add more questions as needed
+]
+
+# Seed questions
+questions_data.each do |question_data|
+    question = Question.create(question_data)
+  
+    # Seed choices for each question
+    choices_data = [
+      { question: 1, body: "高卒か一般大学卒" },
+      { question: 1, body: "福祉大学卒か養成施設卒" },
+      { question: 2, body: "介護福祉士養成施設卒" },
+      { question: 2, body: "福祉系の大学卒か福祉系養成施設卒" },
+      { question: 3, body: "実務経験3年（見込みもあり）" },
+      { question: 3, body: "福祉系高校卒" },
+      # Add more choices as needed
+    ]
+  
+    # Seed choices
+    choices_data.each do |choice_data|
+      Choice.create(choice_data)
+    end
+  end
+  
+  puts "Seed data for questions and choices created successfully."
