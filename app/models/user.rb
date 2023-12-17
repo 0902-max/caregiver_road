0efcfa78d645195de29f08d8d2ea9b24865ad70b
0902-max <moche.0830@icloud.com>
2023-12-17 09:24:14 +0900

    通知機能を各ユーザーのeventと紐つけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :documents
   has_many :answers
+  has_many :events
   has_many :user_past_exam_attempts
   has_many :vocabulary_notes
   has_many :vocabulary_entries
