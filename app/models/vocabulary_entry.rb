5f9c9570e56262777ddfecbca47e71604cb282cb
0902-max <moche.0830@icloud.com>
2024-01-13 15:35:04 +0900

    単語のバリデーション、各ページにヘッダーの配置、外部サイトに遷移する際にjavascriptで確認表示

--- app/models/vocabulary_entry.rb
+++ app/models/vocabulary_entry.rb
@@ -1,4 +1,7 @@
 class VocabularyEntry < ApplicationRecord
   belongs_to :user
   belongs_to :vocabulary_note
+
+  validates :word, presence: true, length: { maximum: 20 }
+  validates :meaning, presence: true
 end
